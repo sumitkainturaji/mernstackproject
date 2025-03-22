a1f0a2b9e2bddfb5415240ab421fd9ea1a7cc6ca
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-03-22 23:39:12 +0530

    Update DB.js

--- Backend/Config/DB.js
+++ Backend/Config/DB.js
@@ -1,17 +1,13 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://kainturasumit190:sumit190@myshop.87vsc.mongodb.net/test',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
-    console.log('DB Connected')
+      'mongodb+srv://kainturasumit190:sumit190@myshop.87vsc.mongodb.net/myDatabaseName'
+    );
+    console.log('DB Connected');
   } catch (error) {
-    console.error('DB Connection Error:', error.message)
-    process.exit(1) // Exit process on failure
+    console.error('DB Connection Error:', error.message);
+    process.exit(1); // Exit process on failure
   }
-}
+};
