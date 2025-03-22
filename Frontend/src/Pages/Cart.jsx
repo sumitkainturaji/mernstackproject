106f348df3819bc252696d882d1d4878e263146a
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-03-22 21:46:10 +0530

    Update Cart.jsx

--- Frontend/src/Pages/Cart.jsx
+++ Frontend/src/Pages/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
   return (
     <div className="max-w-5xl mx-auto p-4">
       <h1 className="text-xl">
-        <Title text1={'YOUR'} text2={'CART'} />{' '}
+        <Title text1={'YOUR'} text2={'CART'} />
       </h1>
 
       {cartData.length > 0 ? (
@@ -29,36 +29,19 @@ const Cart = () => {
                 : `${import.meta.env.VITE_BACKEND_HOST_URL}/${item.image}`
               : assets.contact_img
 
-            console.log('Image URL:', imageUrl)
-
-            return (
-             <div className="max-w-5xl mx-auto p-4">
-      <h1 className="text-xl">
-        <Title text1={'YOUR'} text2={'CART'} />{' '}
-      </h1>
-
-      {cartData.length > 0 ? (
-        <div className="grid gap-6">
-          {cartData.map((item, index) => {
-            const url = 'http://localhost:2000'
-            const imageUrl = `${url}/images/${item.image}`
-
             return (
               <div
                 key={index}
                 className="flex flex-col sm:flex-row items-center justify-between rounded-lg shadow-md bg-white p-4"
               >
-                {imageUrl && (
-                  <div className="w-32 h-32 flex-shrink-0">
-                    <img
-                      src={imageUrl}
-                      alt={item.name}
-                      className="w-full h-full object-cover rounded-md"
-                    />
-                  </div>
-                )}
+                <div className="w-32 h-32 flex-shrink-0">
+                  <img
+                    src={imageUrl}
+                    alt={item.name}
+                    className="w-full h-full object-cover rounded-md"
+                  />
+                </div>
 
-        
                 <div className="flex flex-col flex-1 px-6 text-center sm:text-left">
                   <p className="text-xl font-bold">{item.name}</p>
                   <p className="text-gray-500 text-md">
@@ -69,7 +52,6 @@ const Cart = () => {
                     {item.price}
                   </p>
 
-                
                   {Object.entries(item.sizes).map(([size, quantity]) => (
                     <div
                       key={size}
@@ -89,7 +71,6 @@ const Cart = () => {
                   ))}
                 </div>
 
-          
                 <button
                   onClick={() => removeFromCart(item._id)}
                   className="text-red-500 hover:text-red-700 transition duration-300 p-2"
@@ -98,9 +79,10 @@ const Cart = () => {
                 </button>
               </div>
             )
-          })}       
+          })}
+
           <CartTotal />
-          
+
           <div className="text-center mt-6">
             <button
               onClick={() => navigate('/place-order')}
